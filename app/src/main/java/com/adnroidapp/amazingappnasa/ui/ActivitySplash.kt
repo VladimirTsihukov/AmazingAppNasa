0a4b80edfd43ee9e674bd05c8a24528c07b2355a
Vladimir Tsihukov <tishukooov@gmail.com>
2021-04-12 22:01:11 +0300

    Create splash screen using Activity

---
+++ app/src/main/java/com/adnroidapp/amazingappnasa/ui/ActivitySplash.kt
@@ -0,0 +1,32 @@
+package com.adnroidapp.amazingappnasa.ui
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import com.adnroidapp.amazingappnasa.MainActivity
+import com.adnroidapp.amazingappnasa.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class ActivitySplash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        img_splash.animate()
+            .rotationBy(540f)   //поворот на 750 градусов
+            .setInterpolator(AccelerateDecelerateInterpolator())
+            .setDuration(2000)      //время анимации
+            .setListener(object : Animator.AnimatorListener {
+                override fun onAnimationEnd(p0: Animator?) {
+                    startActivity(Intent(this@ActivitySplash, MainActivity::class.java))
+                    finish()
+                }
+                override fun onAnimationStart(p0: Animator?) {}
+                override fun onAnimationCancel(p0: Animator?) {}
+                override fun onAnimationRepeat(p0: Animator?) {}
+            })
+    }
+}
